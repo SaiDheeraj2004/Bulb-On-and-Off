33178814aa297fe9f4af1eb8a8347e9bbe6dcf66
SaiDheeraj2004 <163376354+SaiDheeraj2004@users.noreply.github.com>
2024-03-19 09:22:06 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,9 @@
+function switchOff() {
+    document.getElementById("bulbImage"). src = "https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg";
+
+}
+
+function switchOn() {
+    document.getElementById("bulbImage"). src = "https://media.geeksforgeeks.org/wp-content/uploads/ONbulb.jpg";
+
+}
